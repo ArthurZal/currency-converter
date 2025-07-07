be8741c5b9441984c8f7fa03e6c7b84963016ba5
Arthur Zall <zallarthur@gmail.com>
2025-07-07 16:51:42 -0300

    feat(Conversion): cria modelo para transferencia de dados json/object

---
+++ src/Conversion.java
@@ -0,0 +1,3 @@
+public record Conversion(String base_code, String target_code,
+                         String conversion_rate, String conversion_result) {
+}
---
+++ src/RatesFilter.java
@@ -0,0 +1,4 @@
+public record RatesFilter(String USD, String AUD, String BGN, String CAD,
+                          String CHF, String CNY, String EGP,String EUR, String GBP){
+
+}
