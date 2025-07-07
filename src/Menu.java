bb44396ebfa2129ed9dff3f7a0202d8492acbab8
Arthur Zall <zallarthur@gmail.com>
2025-07-07 16:52:28 -0300

    feat(Menu): cria classe para representação do menu visual no console

---
+++ src/Menu.java
@@ -0,0 +1,16 @@
+public class Menu {
+
+    public String getMenuInterface() {
+        return """
+                ====== Seja bem-vindo(a) ao Conversor de Moedas ======
+                1) Dólar =>> Peso argentino
+                2) Peso argentino =>> Dólar
+                3) Dólar =>> Euro
+                4) Euro =>> Dólar
+                5) Dolár =>> Real brasileiro
+                6) Real brasileiro =>> Dólar
+                7) Sair                 
+                Escolha uma opção válida:
+                """;
+    }
+}
